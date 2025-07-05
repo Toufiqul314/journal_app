0d496a351c61af1192c59f4442551aa91f251f14
Toufiqul314 <islamtusher4@gmail.com>
2025-07-05 22:20:30 +0600

    Remove unused imports in JournalEntryControllerV2 to clean up the code

--- src/main/java/net/engineeringdigest/journal_app/controller/JournalEntryControllerV2.java
+++ src/main/java/net/engineeringdigest/journal_app/controller/JournalEntryControllerV2.java
@@ -1,14 +1,9 @@
 package net.engineeringdigest.journal_app.controller;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
